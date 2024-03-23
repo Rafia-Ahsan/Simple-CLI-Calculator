a57dff09d0ea29d72b5ea1b184cfb7c2f8456bf1
Rafia-Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-03-24 03:20:03 +0500

    push my CLI Calculator to github

---
+++ index.js
@@ -0,0 +1,31 @@
+#!usr/bin/env node
+import inquirer from "inquirer";
+//Asking questions fron users from inquirer
+let answers = await inquirer.prompt([
+    { massage: "Enter your First Number", type: "number", name: "firstNumber" },
+    { massage: "Enter your Second Number", type: "number", name: "secondNumber" },
+    {
+        massage: "Select one operator to perform your desired operation",
+        type: "list", name: "operator", choices: ["Addition", "Substraction", "Multiplication", "Division", "Modulus"],
+    },
+]);
+console.log(answers);
+//conditional statements we use"if-else"
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "Substraction") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else if (answers.operator === "Modulus") {
+    console.log(answers.firstNumber % answers.secondNumber);
+}
+else {
+    console.log("You've Enter an Invalid Operator, Try Again");
+}
